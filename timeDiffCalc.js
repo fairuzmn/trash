db9101681cd9d1ea89f3b5e4bb2c8249f7167c22
fairuzmn <65823981+fairuzmn@users.noreply.github.com>
2021-01-28 21:01:32 +0700

    Create timeDiffCalc.js

---
+++ timeDiffCalc.js
@@ -0,0 +1,31 @@
+function timeDiffCalc(dateFuture, dateNow) {
+    let diffInMilliSeconds = Math.abs(dateFuture - dateNow) / 1000;
+
+    // calculate days
+    const days = Math.floor(diffInMilliSeconds / 86400);
+    diffInMilliSeconds -= days * 86400;
+    console.log('calculated days', days);
+
+    // calculate hours
+    const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+    diffInMilliSeconds -= hours * 3600;
+    console.log('calculated hours', hours);
+
+    // calculate minutes
+    const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+    diffInMilliSeconds -= minutes * 60;
+    console.log('minutes', minutes);
+
+    let difference = '';
+    if (days > 0) {
+      difference += (days === 1) ? `${days} day, ` : `${days} days, `;
+    }
+
+    difference += (hours === 0 || hours === 1) ? `${hours} hour, ` : `${hours} hours, `;
+
+    difference += (minutes === 0 || hours === 1) ? `${minutes} minutes` : `${minutes} minutes`; 
+
+    return difference;
+  }
+  
+timeDiffCalc(new Date('2020/01/28 11:45'), new Date('2020/01/28 17:00'))
